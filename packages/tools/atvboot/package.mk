8cf3b6b2a542a545a8c9885d61c3f1d8c8d2f2be
chewitt <github@chrishewitt.net>
2016-04-23 10:21:53 +0100

    atvboot: bump to 1c30dab

--- packages/tools/atvboot/package.mk
+++ packages/tools/atvboot/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="atvboot"
-PKG_VERSION="1.1"
+PKG_VERSION="1c30dab"
 PKG_REV="1"
 PKG_ARCH="i386 x86_64"
 PKG_LICENSE="GPL"
