5d9a63128d94802461b420847e31bbcaaa49db1d
Jonas Karlman <jonas@kwiboo.se>
2020-01-16 07:41:15 +0000

    flatbuffers: build and install lib, install flatc in sysroot

--- packages/devel/flatbuffers/package.mk
+++ packages/devel/flatbuffers/package.mk
@@ -8,7 +8,7 @@ PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/google/flatbuffers"
 PKG_URL="https://github.com/google/flatbuffers/archive/v$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="toolchain"
-PKG_DEPENDS_TARGET="toolchain"
+PKG_DEPENDS_TARGET="toolchain flatbuffers:host"
 PKG_LONGDESC="An efficient cross platform serialization library for games and other memory constrained apps."
 
 PKG_CMAKE_OPTS_HOST="-DFLATBUFFERS_CODE_COVERAGE=OFF \
@@ -23,8 +23,8 @@ PKG_CMAKE_OPTS_HOST="-DFLATBUFFERS_CODE_COVERAGE=OFF \
 PKG_CMAKE_OPTS_TARGET="-DFLATBUFFERS_CODE_COVERAGE=OFF \
                        -DFLATBUFFERS_BUILD_TESTS=OFF \
                        -DFLATBUFFERS_INSTALL=ON \
-                       -DFLATBUFFERS_BUILD_FLATLIB=OFF \
-                       -DFLATBUFFERS_BUILD_FLATC=OFF \
+                       -DFLATBUFFERS_BUILD_FLATLIB=ON \
+                       -DFLATBUFFERS_BUILD_FLATC=ON \
                        -DFLATBUFFERS_BUILD_FLATHASH=OFF \
                        -DFLATBUFFERS_BUILD_GRPCTEST=OFF \
                        -DFLATBUFFERS_BUILD_SHAREDLIB=OFF"
@@ -36,4 +36,13 @@ pre_configure_host() {
 post_makeinstall_host() {
   mkdir -p $TOOLCHAIN/bin
     cp -PR $PKG_BUILD/.$HOST_NAME/flatc $TOOLCHAIN/bin
+}
+
+post_makeinstall_target() {
+  rm -rf $INSTALL/usr/bin
+
+  # HACK: we have flatc in $TOOLCHAIN/bin but it seems
+  # the one from sysroot prefix is picked when building hyperion. replace it!
+  rm -f $SYSROOT_PREFIX/usr/bin/flatc
+  cp -PR $TOOLCHAIN/bin/flatc $SYSROOT_PREFIX/usr/bin
 }
