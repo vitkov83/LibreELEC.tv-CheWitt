acac5bbb24d63b8320cc2cf1da5e4b4a3724c0fe
Lukas Rusak <lorusak@gmail.com>
2020-01-26 01:58:42 +0000

    hyperion: correct copyright markings

--- packages/addons/service/hyperion/source/default.py
+++ packages/addons/service/hyperion/source/default.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (C) 2009-2016 Lukas Rusak (lrusak@libreelec.tv)
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 import xbmc
 
