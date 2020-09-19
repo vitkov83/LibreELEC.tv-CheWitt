a0e746c50da8ae6a32402f9fe08a205b05466b21
chewitt <christianshewitt@gmail.com>
2020-09-19 15:30:45 +0000

    amlogic-boot-fip: bump package to latest

--- packages/tools/amlogic-boot-fip/package.mk
+++ packages/tools/amlogic-boot-fip/package.mk
@@ -3,8 +3,8 @@
 
 PKG_NAME="amlogic-boot-fip"
 PKG_LICENSE="nonfree"
-PKG_VERSION="c89bc11d2904e819baed20c7ddba5b5b42a09141"
-PKG_SHA256="63f691f99aee93fe3a917d4b28fc67335a70f14ea3cec544d79983aff62062c9"
+PKG_VERSION="9f21abeb339b7cdd8c66e3e1c94ee9f133ad762e"
+PKG_SHA256="8e0c756992173331327c568023a9c80d34c64c4b27728cf7a49c7338abccb660"
 PKG_SITE="https://github.com/LibreELEC/amlogic-boot-fip"
 PKG_URL="https://github.com/LibreELEC/amlogic-boot-fip/archive/${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="Firmware Image Package (FIP) sources used to sign Amlogic u-boot binaries in LibreELEC images"
