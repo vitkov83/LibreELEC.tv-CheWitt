fec5d3d1bc83e3444ae899298122d1aa0a551290
chewitt <christianshewitt@gmail.com>
2020-09-19 15:21:44 +0000

    amlogic: prevent upgrade from 4.9 kernels

--- projects/Amlogic/bootloader/canupdate.sh
+++ projects/Amlogic/bootloader/canupdate.sh
@@ -4,8 +4,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 # detect legacy kernel installs and abort to prevent upgrades
-if [ "$(uname -r)" = "3.14.29" ]; then
-  echo "Update from 3.14 is not supported!"
+if [ "$(uname -r)" = "3.14.29" ] || [ "$(uname -r)" = "4.9.190" ]; then
+  echo "Update from legacy kernel is not supported!"
   sleep 10
   exit 1
 fi
